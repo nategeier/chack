fe1f13f4c086198c75cad8a00c1db04ff1fbea2a
Nate <nate@interactivebalance.com>
2013-12-07 22:37:13 -0800

    added jade template

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -15,10 +15,16 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+
+
+
+
+
+
 module.exports = {
     
-  
 
+  
 
   /**
    * Overrides for the settings in `config/controllers.js`
