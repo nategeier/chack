0ed78a9bad655ef5167d2f2383d8bd3ed0662b8c
Will Vaughn <willieviseoae@gmail.com>
2013-12-07 22:57:16 -0800

    Hello world for chack app and angular.

--- assets/js/ChackApp/ChackApp.js
+++ assets/js/ChackApp/ChackApp.js
@@ -2,3 +2,12 @@
  * Created by nackjicholson on 12/7/13.
  */
 
+angular.module(
+    'ChackApp',
+    [
+        'ngResource',
+        'ngRoute'
+    ]
+).run(function() {
+    console.log('chack\'d');
+});
